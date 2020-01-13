21127b97c5c4780783c2c43ae862261456e93b3e
LEEHAEHUN <allman956@naver.com>
2020-01-14 02:55:30 +0900

    Coding Test 화면단 구성중

---
+++ src/components/Filter.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div>
+            <b-button v-b-modal.modal-1>필터</b-button>
+
+            <b-modal id="modal-1" title="BootstrapVue">
+                <input type="checkbox" />
+            </b-modal>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
